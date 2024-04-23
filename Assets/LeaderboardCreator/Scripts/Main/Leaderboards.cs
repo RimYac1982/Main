c84b108303306a1540d145b609beed42b95ae936
PrabhnoorKhatkar <prabhnoorkhatkar@gmail.com>
2024-04-23 15:01:15 -0700

    added leaderboard demo code

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference SafariLeaderboard = new LeaderboardReference("ca1878330989b7cd974e2804abf372f9f18b2611c8e659d8e73a472e4946f294");
+    }
+}
